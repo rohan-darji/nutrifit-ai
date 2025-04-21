4b5a0fe1778ab8d04140a8db780aca00f9693f25
rohan-darji <rohandarji2001@gmail.com>
2025-04-21 17:29:04 -0400

    supabase

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -2,8 +2,8 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = "https://psoqkucpxfvtexzpxrct.supabase.co";
-const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBzb3FrdWNweGZ2dGV4enB4cmN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQ5MzA0OTEsImV4cCI6MjA2MDUwNjQ5MX0.86_voeTLn9lUE9I-xLOPCllPAObnL9QFw-UIXgg70ZU";
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_PUBLISHABLE_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 // Import the supabase client like this:
 // import { supabase } from "@/integrations/supabase/client";
